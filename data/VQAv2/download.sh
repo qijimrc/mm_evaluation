43b5473a0a4437f1186edb8540125c0fe2560c6b
qijimrc <qijimrc@github.com>
2023-07-17 13:08:47 +0000

    update

--- data/VQAv2/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Download
-wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip && unzip v2_Questions_Val_mscoco.zip 
-wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip && unzip v2_Annotations_Val_mscoco.zip
-
-# Clear cache
-rm v2_Questions_Val_mscoco.zip v2_Annotations_Val_mscoco.zip
