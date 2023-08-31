64366fcb3d43207fddc12bc6d6d0a68dc983a866
qijimrc <qijimrc@github.com>
2023-08-31 17:06:09 +0800

    add TDIUC with different question types in evaluation

--- data/process_vqav2.py
+++ data/process_vqav2.py
@@ -41,7 +41,10 @@ def select_samples(vqav2_questions_f: List, vqav2_anns_f: List, N: int=500, save
     sorted_qs = sorted(questions['questions'], key=lambda x:x['question_id'])
     sorted_anns = sorted(anns['annotations'], key=lambda x:x['question_id'])
     
-    sampling_ids = np.random.choice(range(len(sorted_qs)), min(N, len(sorted_qs)), replace=False)
+    if N != -1:
+        sampling_ids = np.random.choice(range(len(sorted_qs)), min(N, len(sorted_qs)), replace=False)
+    else:
+        sampling_ids = range(len(sorted_qs))
     tot_succeed = 0
     qa_annotations = []
     for idx in tqdm.tqdm(sampling_ids):
@@ -53,7 +56,7 @@ def select_samples(vqav2_questions_f: List, vqav2_anns_f: List, N: int=500, save
     questions.pop('questions')
     with open(save_f, 'w') as f:
         json.dump(questions, f)
-    return tot_succeed
+    return tot_succeed, len(sorted_qs)
 
 
 
@@ -63,11 +66,12 @@ if __name__ == '__main__':
     parser.add_argument('--vqav2_question_file', default='/nxchinamobile2/shared/instruction_data/MultiInstruct/raw/VQA_V2/v2_OpenEnded_mscoco_val2014_questions.json')
     parser.add_argument('--vqav2_ann_file', default='/nxchinamobile2/shared/instruction_data/MultiInstruct/raw/VQA_V2/v2_mscoco_val2014_annotations.json')
     parser.add_argument('--seed', type=int, default=9271)
-    parser.add_argument('--N', type=int, default=500, help='The number of examples for sampling.')
+    parser.add_argument('--N', type=int, default=-1, help='The number of examples for sampling, where -1 means selecting the full set.')
     parser.add_argument('--save_file', default='/nxchinamobile2/shared/instruction_data/evaluation/vqav2_sampled.jsonl')
     args = parser.parse_args()
 
     np.random.seed(args.seed)
 
-    select_samples(args.vqav2_question_file, args.vqav2_ann_file, args.N, args.save_file)
+    tot_select, tot = select_samples(args.vqav2_question_file, args.vqav2_ann_file, args.N, args.save_file)
+    print(f"Selected {tot_select} samples from total of {tot}")
     
